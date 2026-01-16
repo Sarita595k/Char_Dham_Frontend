f9315637e33d9957c415d594539408f3342ac6c1
sarita595k <sarita595k@gmail.com>
2026-01-16 14:05:57 +0530

    header image align

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,26 @@
 import headerImg from "../assets/Char Dham Yatra Family Package (1).png"
 import { Image } from "react-bootstrap"
 import mobileImage from "../assets/Header/mobile1.jpg"
+import styled from "styled-components"
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
 export const Header = () => {
     return (
-        <>
+        <HeaderWrapper>
             <picture>
                 <source srcSet={mobileImage} media="(max-width:660px)" alt="header image" style={{ objectFit: "cover" }} />
-                <Image src={headerImg} alt="header image" fluid />
+                <img src={headerImg} alt="header image" />
             </picture>
-        </>
+        </HeaderWrapper>
     )
 }
 

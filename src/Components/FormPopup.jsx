cd887e2a84fb44e57115ef482f6006c93779c330
sarita595k <sarita595k@gmail.com>
2026-01-20 22:18:41 +0530

    form design completed

--- src/Components/FormPopup.jsx
+++ src/Components/FormPopup.jsx
@@ -1,13 +1,93 @@
 import { useState } from 'react';
+import styled from 'styled-components';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import logo from '../assets/Logo/YugenIndiaLogo.png'
+import bgImage from "../assets/Background-color/bg2.jpg"
 
+const ModelWrapper = styled(Modal)`
+.modal-dialog{
+max-width:clamp(65%, 45vw, 75%);
+margin:auto;}
+
+.modal-header{
+display:flex;
+flex-direction:column;
+align-items:center;
+position:relative;
+color:#7E4555;
+margin-top:-2rem;
+// background-image:url(${bgImage});
+background-size:cover;
+border-bottom:none;
+}
+
+.modal-title{
+font-size:clamp(1.7rem,1.6vw, 10.7rem);
+}
+
+.btn-close{
+position:absolute;
+top:4rem;
+right:3rem;
+}
+
+// .modal-footer{
+// display:flex;
+// justify-content:space-between;
+// }
+`
+
+const LogoImage = styled.img`
+height:10rem;
+width:10rem;
+`
+
+const ModalBody = styled(Modal.Body)`
+padding:1rem;
+`
+
+const FormWrapper = styled.form`
+input{
+font-size:clamp(1.2rem, 1.6vw, 2rem);
+padding:clamp(.7rem,1.6vw,1.6rem);
+margin-top:clamp(1rem,1.6vw,2rem);
+}
+`
+
+const CloseBtn = styled(Button)`
+font-size:1.3rem;
+background-color:white;
+   color:#7E4555;
+  border:1px solid #7E4555;
+  padding:.7rem 2rem;
+  cursor:pointer;
+  transition:all .5s ease-in-out;
+
+  &:hover{
+  background-color:#7E4555;
+  color:white;
+  }
+`
+const WhatsAppBtn = styled(Button)`
+font-size:1.3rem;
+  background-color:#7E4555;
+  color:white;
+  border:none;
+  padding:.7rem 2rem;
+  cursor:pointer;
+  transition:all .5s ease-in-out;
+
+  &:hover{
+    background-color:white;
+    color:#7E4555;
+  }`
 export const FormPopup = ({ show, onClose }) => {
     const phone = 918770872054
     const [formData, setFormData] = useState({
         userName: "",
         phoneNumber: "",
-        preferredMonth: ""
+        email: ""
 
     })
 
@@ -25,7 +105,7 @@ Hi, I want Deluxe Char Dham Package.
 
 Name: ${formData.userName}
 Phone: ${formData.phoneNumber}
-Preferred Month: ${formData.preferredMonth}
+Preferred Month: ${formData.email}
     `;
 
         const whatsappLink = `https://wa.me/${phone}?text=${encodeURIComponent(
@@ -36,27 +116,28 @@ Preferred Month: ${formData.preferredMonth}
         onClose();
     };
     return (
-        <Modal show={show} onHide={onClose} centered>
+        <ModelWrapper show={show} onHide={onClose} centered>
             <Modal.Header closeButton>
+                <LogoImage src={logo} />
                 <Modal.Title>Request Itinerary</Modal.Title>
             </Modal.Header>
 
-            <Modal.Body>
-                <form>
-                    <input className="form-control mb-2" name="userName" value={formData.userName} placeholder="Your Name" onChange={handleChange} />
-                    <input className="form-control mb-2" name="phoneNumber" value={formData.phoneNumber} placeholder="Phone Number" onChange={handleChange} />
-                    <input className="form-control mb-2" name="preferredMonth" value={formData.preferredMonth} placeholder="Preferred month" onChange={handleChange} />
-                </form>
-            </Modal.Body>
+            <ModalBody>
+                <FormWrapper>
+                    <input type='text' className="form-control mb-2" name="userName" value={formData.userName} placeholder="Your Name" onChange={handleChange} required />
+                    <input type='tel' className="form-control mb-2" name="phoneNumber" value={formData.phoneNumber} pattern="[6-9]{1}[0-9]{9}" maxLength="10" placeholder="Phone Number" onChange={handleChange} required />
+                    <input type='email' className="form-control mb-2" name="email" value={formData.email} placeholder="Your Email" onChange={handleChange} required />
+                </FormWrapper>
+            </ModalBody>
 
             <Modal.Footer>
-                <Button variant="secondary" onClick={onClose}>
+                <CloseBtn variant="secondary" onClick={onClose}>
                     Close
-                </Button>
-                <Button variant="primary" onClick={handleSubmit}>
+                </CloseBtn>
+                <WhatsAppBtn variant="primary" onClick={handleSubmit} target='_blank'>
                     Submit on Whatsapp
-                </Button>
+                </WhatsAppBtn>
             </Modal.Footer>
-        </Modal>
+        </ModelWrapper>
     );
 }

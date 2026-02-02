fedf5405bd4ca8222581b23d9dc9761f337c7a02
sarita595k <sarita595k@gmail.com>
2026-02-02 10:52:51 +0530

    small changes

--- src/Components/CustomizeReg.jsx
+++ src/Components/CustomizeReg.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { SubHeading } from "./SubHeading"
 const CustomizeReg = () => {
     const [details, setDetails] = useState({
@@ -6,14 +6,32 @@ const CustomizeReg = () => {
         email: "",
         mobileNumber: ""
     })
+
+    const handleChange = (event) => {
+        const { name, value } = event.target
+        setDetails((prev) => ({
+            ...prev,
+            [name]: value
+        }))
+        console.log(details.name, details.email, details.mobileNumber)
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setDetails({
+            name: "",
+            email: "",
+            mobileNumber: ""
+        })
+
+    }
     return (
         <div>
             {/* subheading called  */}
             <SubHeading subHeading="customize your holiday package with our travel experts" />
-            <form>
-                <input type="text" name="name" value={details.name} placeholder='Enter your name' />
-                <input type="email" name="email" value={details.email} placeholder='enter your email' />
-                <input type="tel" name="mobileNumber" value={details.mobileNumber} placeholder='enter your mobile number' />
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" value={details.name} placeholder='Enter your name' onChange={handleChange} />
+                <input type="email" name="email" value={details.email} placeholder='enter your email' onChange={handleChange} />
+                <input type="tel" name="mobileNumber" value={details.mobileNumber} placeholder='enter your mobile number' onChange={handleChange} />
                 <button>Submit</button>
             </form>
         </div>

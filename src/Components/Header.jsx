8b661760da3c364b8f67c51501fab85bcead9c74
sarita595k <sarita595k@gmail.com>
2026-02-03 19:54:56 +0530

    header image changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import headerImg from "../assets/Char Dham Yatra Family Package (1).png"
 // import { Image } from "react-bootstrap"
-import mobileImage from "../assets/Header/mobile1.jpg"
+import mobileImage from "../assets/Header/Header_Mobile.png"
 import styled from "styled-components"
 
 const HeaderWrapper = styled.div`

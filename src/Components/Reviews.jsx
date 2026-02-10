0e05dc2ee9afb979e89e9a496774f828edaeaebe
sarita595k <sarita595k@gmail.com>
2026-02-10 15:22:38 +0530

    reviews carousel completed

--- src/Components/Reviews.jsx
+++ src/Components/Reviews.jsx
@@ -2,64 +2,100 @@ import styled from 'styled-components'
 import Carousel from 'react-bootstrap/Carousel'
 import { Card } from 'react-bootstrap'
 import { Heading } from './Heading'
+import { Reviews } from './ReviewsList'
 
 const Div = styled.div`
-// display:flex;
-// flex-direction:column;
-background-color:transaprent;
+width:100%;
 `
+const CenterWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
 const CarouselContainer = styled(Carousel)`
-// background-color:transparent;
+.carousel-caption{
+ position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0; /* optional, can remove if you want captions only on top/bottom */
+    pointer-events: none; /* allow clicks to go through except buttons */
+    z-index: 2;
+  }
+
+  .carousel-indicators {
+    z-index: 5; /* above overlay */
+  }
+
+  .carousel-control-prev,
+  .carousel-control-next {
+    z-index: 5; /* above overlay */
+  }
+
+  .carousel-indicators button {
+    background-color: #fff; /* inactive dot */
+  }
+
+  .carousel-indicators .active {
+    background-color: #ffcc00; /* active dot */
+  }
+
+  .carousel-control-prev-icon,
+  .carousel-control-next-icon {
+    filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
+    /* this makes the arrow white */
+  }
+    .carousel-control-prev {
+  left: -2rem; /* move left arrow outside the content */
+}
+
+.carousel-control-next {
+  right: -2rem; /* move right arrow outside the content */
+}
 `
 
-
-const Reviews = () => {
+const CardContainer = styled(Card)`
+width:80%;
+height:25vh;
+background-color:transparent;
+border:none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`
+const CardTitle = styled(Card.Title)`
+font-size:clamp(1.3rem,2vw,2.1rem);
+color:#CC3366;
+`
+const CardText = styled(Card.Text)`
+font-size:clamp(1.2rem,2vw,2.1rem);
+`
+const ReviewsComp = () => {
     return (
         <Div>
             <Heading heading={"Testimonials"} />
             <CarouselContainer>
-                <Carousel.Item>
-                    <Card>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>1. Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            {/* <Button variant="primary">Go somewhere</Button> */}
-                        </Card.Body>
-                    </Card>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Card >
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>2. Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            {/* <Button variant="primary">Go somewhere</Button> */}
-                        </Card.Body>
-                    </Card>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <Card >
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>3. Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            {/* <Button variant="primary">Go somewhere</Button> */}
-                        </Card.Body>
-                    </Card>
-                </Carousel.Item>
+                {Reviews.map(review =>
+                    <Carousel.Item key={review.id}>
+                        <CenterWrapper>
+                            <CardContainer>
+                                <Card.Body>
+                                    <CardTitle>{review.name}</CardTitle>
+                                    <CardText>
+                                        {review.description}
+                                    </CardText>
+                                    {/* <Button variant="primary">Go somewhere</Button> */}
+                                </Card.Body>
+                            </CardContainer>
+                        </CenterWrapper>
+                    </Carousel.Item>
+                )}
+
             </CarouselContainer>
         </Div>
     )
 }
 
-export default Reviews
+export default ReviewsComp

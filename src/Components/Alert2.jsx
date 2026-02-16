59e835d949ef2ac656d5a381b8b6b4a8f6db4ed6
sarita595k <sarita595k@gmail.com>
2026-02-16 12:12:34 +0530

    alert 2 text changed

--- src/Components/Alert2.jsx
+++ src/Components/Alert2.jsx
@@ -7,13 +7,14 @@ background-color:#A3A333;
 padding:.7rem`
 
 const H2 = styled.h2`
-font-size:1.2rem;
+font-size:clamp(1.2rem,1.6vw,2rem);
+text-transform:capitalize;
 color:#4A2C2A;
 `
 const Alert2 = () => {
     return (
         <Div>
-            <H2>Ask for Discount while booking for Group packages upto 30%</H2>
+            <H2>we offer a family referral advantage for eligible bookings.</H2>
         </Div>)
 }
 export default Alert2

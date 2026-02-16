eda869d043f7db66c478018f9f5cc666297d777b
sarita595k <sarita595k@gmail.com>
2026-02-16 11:45:02 +0530

    lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
-import { LandingPage } from './Pages/LandingPage'
-import { Route, Routes } from 'react-router-dom';
-import PrivacyPolicy from "./Pages/PrivacyPolicy"
-import RefundPolicy from "./Pages/RefundPolicy"
-import TermsCondition from "./Pages/TermsCondition"
-import WhatsTrendingPage from './Components/Itinerary';
-import TrendingPage from './Pages/TrendingPage';
-import { Header } from './Components/Header';
-import { Alert1 } from './Components/Alert1';
-import { Alert2 } from './Components/Alert2';
-import Footer from './Components/Footer';
-import AllRightsReserved from './Components/AllRightsReserved';
-import ScrollToTop from './Components/ScrollToTop';
-import ThankYou from './Components/ThankYou';
+const LandingPage = lazy(() => import("./Pages/LandingPage"));
+const PrivacyPolicy = lazy(() => import("./Pages/PrivacyPolicy"));
+const RefundPolicy = lazy(() => import("./Pages/RefundPolicy"));
+const TermsCondition = lazy(() => import("./Pages/TermsCondition"));
+const TrendingPage = lazy(() => import("./Pages/TrendingPage"));
+const ThankYou = lazy(() => import("./Components/ThankYou"));
+
+const Header = lazy(() => import("./Components/Header"));
+const Footer = lazy(() => import("./Components/Footer"));
+const Alert1 = lazy(() => import("./Components/Alert1"));
+const Alert2 = lazy(() => import("./Components/Alert2"));
+const AllRightsReserved = lazy(() => import("./Components/AllRightsReserved"));
 
 
 const App = () => {
   return (
     <>
       <ScrollToTop />
-      <Alert1 />
-      <Alert2 />
-      <Header />
-      <Routes>
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/privacy' element={<PrivacyPolicy />} />
-        <Route path='/refund' element={<RefundPolicy />} />
-        <Route path='/terms' element={<TermsCondition />} />
-        <Route path='/trending' element={<TrendingPage />} />
-        <Route path="/thankYou" element={<ThankYou />} />
-      </Routes>
-      <Footer />
-      <AllRightsReserved />
+      <Suspense fallback={null}>
+        <Alert1 />
+        <Alert2 />
+        <Header />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/privacy' element={<PrivacyPolicy />} />
+          <Route path='/refund' element={<RefundPolicy />} />
+          <Route path='/terms' element={<TermsCondition />} />
+          <Route path='/trending' element={<TrendingPage />} />
+          <Route path="/thankYou" element={<ThankYou />} />
+        </Routes>
+        <Footer />
+        <AllRightsReserved />
+      </Suspense>
     </>
   )
 }
